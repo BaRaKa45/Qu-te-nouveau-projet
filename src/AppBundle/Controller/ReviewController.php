f1afd6c66e2de63bc9cc38ae4fb8dce1c9f3ece5
Anthony Sottejeau <anthony.sottejeau@gmail.com>
2018-04-30 16:52:48 +0200

    review

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Review;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Reservation controller.
+ *
+ * @Route("review")
+ */
+class ReviewController extends Controller
+{
+    /**
+     * Lists all reservation entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
+    }
+
+    /**
+     * Lists all reservation entities.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction()
+    {
+        $review = new Review();
+
+        return $this->render('review/new.html.twig', array(
+            'review' => $review,
+        ));
+    }
+
+}
